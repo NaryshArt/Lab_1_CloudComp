a364283fbf4ad33b4087457668710e01823562ce
NaryshArt <168580312+NaryshArt@users.noreply.github.com>
2026-04-23 09:37:10 +0400

    Fix formatting in BaseIntegrationTest.java

--- src/test/java/org/todolist/project/BaseIntegrationTest.java
+++ src/test/java/org/todolist/project/BaseIntegrationTest.java
@@ -20,7 +20,7 @@ import org.todolist.project.repository.TaskRepository;
 @Transactional
 @Testcontainers
 public abstract class BaseIntegrationTest {
-
+ 
     @Container
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
 
